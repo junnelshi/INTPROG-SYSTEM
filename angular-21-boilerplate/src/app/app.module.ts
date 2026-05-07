59f9c66b266ab08b305fd62f9ea1f2ecfb6f4c65
Ranz Junnel Lapis <lapisranzjunnel@gmail.com>
2026-05-07 21:56:02 +0800

    Testing the fakebackend and the realbackend

--- angular-21-boilerplate/src/app/app.module.ts
+++ angular-21-boilerplate/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
-
+// import { fakeBackendProvider } from './_helpers/fake-backend';
 import { AppRoutingModule } from './app-routing.module';
 import { JwtInterceptor, ErrorInterceptor, appInitializer } from './_helpers';
 import { AccountService } from './_services';
@@ -26,6 +26,7 @@ import { HomeComponent } from './home';
         { provide: APP_INITIALIZER, useFactory: appInitializer, multi: true, deps: [AccountService] },
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+        //fakeBackendProvider
     ],
     bootstrap: [AppComponent]
 })
