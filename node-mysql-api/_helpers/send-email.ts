057032035be8a3457ea80386f9c61fe164fa0f81
Ranz Junnel Lapis <lapisranzjunnel@gmail.com>
2026-05-13 07:24:38 +0800

    fix: use official Brevo package

--- node-mysql-api/_helpers/send-email.ts
+++ node-mysql-api/_helpers/send-email.ts
@@ -1,18 +1,17 @@
-import * as SibApiV3Sdk from '@sendinblue/client';
+import * as Brevo from '@getbrevo/brevo';
 
-const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-apiInstance.setApiKey(SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY!);
+const apiInstance = new Brevo.TransactionalEmailsApi();
+apiInstance.setApiKey(Brevo.TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY!);
 
 export default async function sendEmail({ to, subject, html }: any) {
     console.log('Sending email to:', to);
 
-    const sendSmtpEmail = {
-        to: [{ email: to }],
-        sender: { email: 'lapisranzjunnel@gmail.com', name: 'INTPROG System' },
-        subject,
-        htmlContent: html
-    };
+    const sendSmtpEmail = new Brevo.SendSmtpEmail();
+    sendSmtpEmail.to = [{ email: to }];
+    sendSmtpEmail.sender = { email: 'lapisranzjunnel@gmail.com', name: 'INTPROG System' };
+    sendSmtpEmail.subject = subject;
+    sendSmtpEmail.htmlContent = html;
 
     const result = await apiInstance.sendTransacEmail(sendSmtpEmail);
-    console.log('Email sent successfully:', result);
+    console.log('Email sent successfully:', result.response.statusCode);
 }
