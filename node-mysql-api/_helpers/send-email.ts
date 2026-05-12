89168d11d57c048f62fde0d366317fd7f1199a34
Ranz Junnel Lapis <lapisranzjunnel@gmail.com>
2026-05-12 22:23:54 +0800

    fix smtp transport type error

--- node-mysql-api/_helpers/send-email.ts
+++ node-mysql-api/_helpers/send-email.ts
@@ -4,7 +4,6 @@ export default async function sendEmail({ to, subject, html, from = process.env.
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: Number(process.env.SMTP_PORT),
-        family: 4,
         connectionTimeout: 5000,
         greetingTimeout: 5000,
         socketTimeout: 5000,
@@ -15,7 +14,7 @@ export default async function sendEmail({ to, subject, html, from = process.env.
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
         }
-    });
+    } as any);
 
     await transporter.sendMail({ from, to, subject, html });
 }
