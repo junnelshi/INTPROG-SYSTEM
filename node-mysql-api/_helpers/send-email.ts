3a64929a374f5aac0b207fbbfde3f3005d31a8f8
Ranz Junnel Lapis <lapisranzjunnel@gmail.com>
2026-05-13 07:29:09 +0800

    fix: use require for Brevo import

--- node-mysql-api/_helpers/send-email.ts
+++ node-mysql-api/_helpers/send-email.ts
@@ -1,4 +1,4 @@
-import SibApiV3Sdk from '@getbrevo/brevo';
+const SibApiV3Sdk = require('@getbrevo/brevo');
 
 const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 apiInstance.setApiKey(SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY!);
