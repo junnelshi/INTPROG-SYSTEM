727c3bdbd5193a78ce51a506e7b853162a77ac1f
Ranz Junnel Lapis <lapisranzjunnel@gmail.com>
2026-05-12 13:26:33 +0800

    Fix Swagger UI using CDN assets for Vercel

--- node-mysql-api/_helpers/swagger.js
+++ node-mysql-api/_helpers/swagger.js
@@ -10,6 +10,13 @@ const __dirname = path.dirname(__filename);
 
 const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
 
-router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+router.use('/', swaggerUi.serve);
+router.get('/', swaggerUi.setup(swaggerDocument, {
+  customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui.min.css',
+  customJs: [
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-bundle.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-standalone-preset.min.js'
+  ]
+}));
 
 export default router;
