852474a8d9771d2efe1f6e059c5d4f724af9b953
Ranz Junnel Lapis <lapisranzjunnel@gmail.com>
2026-05-13 07:26:40 +0800

    fix: correct Brevo import style

--- node-mysql-api/_helpers/send-email.ts
+++ node-mysql-api/_helpers/send-email.ts
@@ -1,12 +1,12 @@
-import * as Brevo from '@getbrevo/brevo';
+import SibApiV3Sdk from '@getbrevo/brevo';
 
-const apiInstance = new Brevo.TransactionalEmailsApi();
-apiInstance.setApiKey(Brevo.TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY!);
+const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+apiInstance.setApiKey(SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY!);
 
 export default async function sendEmail({ to, subject, html }: any) {
     console.log('Sending email to:', to);
 
-    const sendSmtpEmail = new Brevo.SendSmtpEmail();
+    const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
     sendSmtpEmail.to = [{ email: to }];
     sendSmtpEmail.sender = { email: 'lapisranzjunnel@gmail.com', name: 'INTPROG System' };
     sendSmtpEmail.subject = subject;
