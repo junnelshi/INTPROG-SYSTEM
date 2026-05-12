74b279d3018a02178db8458ede3f242d5f9d3c0d
Ranz Junnel Lapis <lapisranzjunnel@gmail.com>
2026-05-13 07:34:39 +0800

    fix: use default export for Brevo classes

--- node-mysql-api/_helpers/send-email.ts
+++ node-mysql-api/_helpers/send-email.ts
@@ -2,13 +2,13 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const SibApiV3Sdk = require('@getbrevo/brevo');
 
-const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-apiInstance.setApiKey(SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY!);
+const apiInstance = new SibApiV3Sdk.default.TransactionalEmailsApi();
+apiInstance.setApiKey(SibApiV3Sdk.default.TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY!);
 
 export default async function sendEmail({ to, subject, html }: any) {
     console.log('Sending email to:', to);
 
-    const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+    const sendSmtpEmail = new SibApiV3Sdk.default.SendSmtpEmail();
     sendSmtpEmail.to = [{ email: to }];
     sendSmtpEmail.sender = { email: 'lapisranzjunnel@gmail.com', name: 'INTPROG System' };
     sendSmtpEmail.subject = subject;
