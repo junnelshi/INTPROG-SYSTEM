c35f417ca288a5dda393b2568bb5602431a56a8a
Ranz Junnel Lapis <lapisranzjunnel@gmail.com>
2026-05-12 13:23:59 +0800

    Fix swagger.js absolute path for Vercel

--- node-mysql-api/_helpers/swagger.js
+++ node-mysql-api/_helpers/swagger.js
@@ -2,7 +2,13 @@ import express from 'express';
 const router = express.Router();
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
-const swaggerDocument = YAML.load('./swagger.yaml');
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
 
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
