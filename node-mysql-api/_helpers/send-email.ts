46bef431a23b7c52c193c32e7873b0f494b2d66a
Ranz Junnel Lapis <lapisranzjunnel@gmail.com>
2026-05-12 22:41:58 +0800

    fix: use env vars for SMTP credentials

--- node-mysql-api/_helpers/send-email.ts
+++ node-mysql-api/_helpers/send-email.ts
@@ -1,18 +1,15 @@
 import nodemailer from 'nodemailer';
 
-export default async function sendEmail({ to, subject, html, from = process.env.EMAIL_FROM }: any) {
+export default async function sendEmail({ to, subject, html, from }: any) {
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: Number(process.env.SMTP_PORT),
-        secure: true,
-        connectionTimeout: 10000,
-        greetingTimeout: 10000,
-        socketTimeout: 10000,
+        secure: false,
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
         }
     } as any);
 
-    await transporter.sendMail({ from, to, subject, html });
+    await transporter.sendMail({ from: from || process.env.EMAIL_FROM, to, subject, html });
 }
